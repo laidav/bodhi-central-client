c18473c68b3ddf1a98243b8f536d0a1e0a520b1e
DavidLai <david.lai@postbeyond.com>
2019-03-19 14:35:22 -0400

    added verify token endpoint in authResource

--- src/services/resources/authResource.js
+++ src/services/resources/authResource.js
@@ -7,7 +7,14 @@ const authResource = {
       method: "get",
       url: Constants.API_V1 + "/token",
       auth: { username, password }
-    })
+    });
+  },
+  verifyToken(token) {
+    return axios({
+      method: "get",
+      url: Constants.API_V1 + "/verify-token",
+      auth: { username: token }
+    });
   }
 };
 
