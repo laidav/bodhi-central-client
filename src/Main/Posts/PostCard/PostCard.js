3c2079611b98791c9dd4284eb6927b2da3a3c329
David Lai <laidav@gmail.com>
2019-04-21 10:27:44 -0400

    styling body section of post-card

--- src/Main/Posts/PostCard/PostCard.js
+++ src/Main/Posts/PostCard/PostCard.js
@@ -3,25 +3,30 @@ import List from "../../../common/List/List";
 import SubjectTag from "./SubjectTag/SubjectTag";
 import "./PostCard.scss";
 import { NavLink } from "react-router-dom";
+import buddhistPath from "images/buddhist-path.jpg";
 
-const PostCard = ({ data }) => {
+const PostCard = ({ data: post }) => {
   return (
-    <div className="post-card" key={data.id}>
-      <p>Post Card</p>
-      <p>Title: {data.title}</p>
-      <p>Description: {data.description}</p>
-      <a href={data.link} target="_blank" rel="noopener noreferrer">
-        Source: {data.link}
+    <div className="post-card" key={post.id}>
+      <h5 className={"post-card__title"}>{post.title}</h5>
+      <div className="post-card__body">
+        <div className={"post-card__description"}>{post.description}</div>
+        <div className={"post-card__image"}>
+          <img src={buddhistPath} title={"path"} alt={"path"} />
+        </div>
+      </div>
+      <a href={post.link} target="_blank" rel="noopener noreferrer">
+        Source: {post.link}
       </a>
-      <p>Author: {data.author.username}</p>
+      <p>Author: {post.author.username}</p>
       <p>Subjects</p>
       <List
         className="post-card__subject-tags"
         component={SubjectTag}
         uniqueKey="id"
-        list={data.subjects}
+        list={post.subjects}
       />
-      <NavLink to={`/post/${data.id}`}>Go to {data.title}</NavLink>
+      <NavLink to={`/post/${post.id}`}>Go to {post.title}</NavLink>
     </div>
   );
 };
