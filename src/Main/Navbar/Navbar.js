32737f9619596374ae38d99e823d11ea24690abd
David Lai <laidav@gmail.com>
2019-04-21 08:36:06 -0400

    added media query break points and container widths

--- src/Main/Navbar/Navbar.js
+++ src/Main/Navbar/Navbar.js
@@ -5,6 +5,9 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <nav className={"main-nav"}>
+      <NavLink to="/" className={"main-nav__logo"}>
+        BodhiCentral.com
+      </NavLink>
       <ul>
         <li className={"main-nav__item"}>
           <NavLink activeClassName="active" to="/">
