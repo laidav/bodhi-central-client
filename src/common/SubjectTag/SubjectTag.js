65538a4843f936a80eb5910394781b0953cea48f
Dave <laidav@gmail.com>
2019-05-08 11:10:01 -0400

    styled subject tags and adding padding on post-card

--- src/common/SubjectTag/SubjectTag.js
+++ src/common/SubjectTag/SubjectTag.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SubjectTag.scss";
 
 const SubjectTag = ({ data }) => {
-  return <div>{data.name}</div>;
+  return <span className={"subject-tag"}>{data.name}</span>;
 };
 
 export default SubjectTag;
