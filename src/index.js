6828a8d04ea19e803736ac8991fe9bc586fb9840
Dave <laidav@gmail.com>
2019-06-26 12:33:14 -0400

    apply thunk middle ware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
