e96deaaccf543a24ed103a164d21e55cec0f2daf
Dave <laidav@gmail.com>
2019-06-27 21:55:18 -0400

    remove unnesscary PostsPracticeContainer container component

--- src/Main/Posts/Posts.js
+++ src/Main/Posts/Posts.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./Posts.scss";
 import { connect } from "react-redux";
 import { getPostsPractices } from "actions";
-import PostsPracticeContainer from "./PostsPracticeContainer/PostsPracticeContainer";
+import PracticesContainer from "common/PracticesContainer/PracticesContainer";
 import PostsContainer from "./PostsContainer/PostsContainer";
 import SubjectTab from "./SubjectTab/SubjectTab";
 import * as Constants from "services/constantsSrvc";
 
 const mapStateToProps = state => ({
-  pagination: state.pagination.postsPractices
+  pagination: state.pagination.postsPractices,
+  practices: state.pagination.postsPractices.ids.map(
+    practiceId => state.entities.practices[practiceId]
+  )
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -37,7 +40,7 @@ class Posts extends Component {
 
   render() {
     const { activeSubjectTab } = this.state;
-    const { match } = this.props;
+    const { match, practices, pagination } = this.props;
     const { getPractices } = this;
 
     return (
@@ -71,7 +74,11 @@ class Posts extends Component {
           </div>
         </div>
         <div className={"posts__side-bar"}>
-          <PostsPracticeContainer getPractices={getPractices} />
+          <PracticesContainer
+            getPractices={getPractices}
+            practices={practices}
+            pagination={pagination}
+          />
         </div>
       </div>
     );
