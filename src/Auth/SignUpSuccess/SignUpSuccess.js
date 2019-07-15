f0b64a525455fe76f657cf0322d957f8533cdbbf
David Lai <laidav@gmail.com>
2019-07-14 20:48:50 -0400

    updated styles for h1

--- src/Auth/SignUpSuccess/SignUpSuccess.js
+++ src/Auth/SignUpSuccess/SignUpSuccess.js
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const SignUpSuccess = () => (
   <div className={"sign-up-success"}>
-    <p>You have successfully created an account.</p>
+    <h1>Success!</h1>
+    <p>You have created an account.</p>
     <p>Please check your email to activate your account.</p>
     <p>
       Then proceed to <NavLink to={"/sign-in"}>Login</NavLink>
